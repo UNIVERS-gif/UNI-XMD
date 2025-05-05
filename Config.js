3f75b5f5ecffb78c2909ea9be43896b22a3788ca
UNIVERS-gif <univesrmd99@gmail.com>
2025-05-05 19:44:43 +0530

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  OWNER_NUMBER: "94740021158",
+  BOT_NAME: "UNI-XMD",
+  SESSION_ID: "your-session-id-here",
+  AUTO_STATUS_SEEN: true,
+  AUTO_VOICE_REPLY: true,
+  LANGUAGE: "EN",
+  FOOTER: "POWERED BY NETHUM OFC",
+  PREFIX: ".",
+  REACT: true,
+  AUTO_READ: true,
+  AUTO_TYPING: true,
+  AUTO_RECORD: false,
+  BLOCK_CALLS: true,
+  LOGO: "https://telegra.ph/file/8edb1605f6d6bb6fd6be0.jpg"
+};
